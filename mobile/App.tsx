18ff7221c4df227b04b131dc0496f70e46916667
Luiz Oliveira <luizoliveira2808@gmail.com>
2022-11-02 14:38:19 -0300

    Aula 03

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,29 @@
-import { NativeBaseProvider, StatusBar } from "native-base";
-import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { THEME } from "./src/styles/theme";
-import { Loading } from "./src/components/Loading";
-import { SignIn } from "./src/screens/SignIn";
+import { Loading } from './src/components/Loading';
+
+import { THEME } from './src/styles/theme';
+import { AuthContextProvider } from './src/contexts/AuthContext';
+import { SignIn } from './src/screens/SignIn';
+import { New } from './src/screens/New';
+import { Find } from './src/screens/Find';
+import { Pools } from './src/screens/Pools';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular, Roboto_500Medium, Roboto_700Bold
-  });
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-
-      {!fontsLoaded ? <Loading /> : <SignIn />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle='light-content'
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
